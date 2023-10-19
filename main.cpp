68cae54856340320e4e61b5db908b32efa722b86
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-19 13:59:11 -0400

    Completed countVowel function

--- main.cpp
+++ main.cpp
@@ -10,10 +10,24 @@ using namespace std;
 
 int main(void){
     int countCharacter(string str);
+    int countVowel(string str);
+    
     
     
 }
 
 int countCharacter(string str){
     return str.length();
+}
+
+int countVowel(string str){
+    int vowels = 0;
+    for(int i = 1; i <= countCharacter(str); i++){
+        string first;
+        first = str.substr(countCharacter(str) - i);
+        if(first.front() == 'a' || first.front() == 'A' || first.front() == 'e' || first.front() == 'E' || first.front() == 'i' || first.front() == 'I' || first.front() == 'o' || first.front() == 'O' || first.front() == 'u' || first.front() == 'U'){
+            vowels++;
+        }
+    }
+    return vowels;
 }
